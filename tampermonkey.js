97ed3e79402d7b22af7b7066eb2bb5ba17452907
Sascha <speckmaier58@gmail.com>
2024-05-08 17:28:43 +0200

    Create tampermonkey.js

---
+++ tampermonkey.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Stämme Bot
+// @namespace    https://raw.githubusercontent.com/Jimmy-653/staemme-public
+// @version      0.0.1
+// @description  Create a "Bot" menu in online game Tribal Wars to automate various game actions.
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @author       Jimmy653
+// @match        https://*.die-staemme.de/*
+// @updateURL	   https://raw.githubusercontent.com/Jimmy-653/staemme-public/main/epm.js
+// @downloadURL  https://raw.githubusercontent.com/Jimmy-653/staemme-public/main/epm.js
+// @require		   https://raw.githubusercontent.com/Jimmy-653/staemme-public/main/staemme.js
+// @homepage     https://raw.githubusercontent.com/Jimmy-653/staemme-public/main/README.md
+// @supportURL   https://discordapp.com/users/266279831618977792
+// ==/UserScript==
+
+const {initStaemmeBot} = staemme;
+initStaemmeBot();

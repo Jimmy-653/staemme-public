3e5838221fd9e95b42761635037419e3c5fd3c6c
Jimmy <speckmaier58@gmail.com>
2024-05-09 09:11:57 +0000

    create staemme.js

--- tampermonkey.js
+++ tampermonkey.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Stämme Bot
 // @namespace    https://raw.githubusercontent.com/Jimmy-653/staemme-public
-// @version      0.0.1
+// @version      0.0.3
 // @description  Create a "Bot" menu in online game Tribal Wars to automate various game actions.
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @author       Jimmy653
 // @match        https://*.die-staemme.de/*
-// @updateURL	   https://raw.githubusercontent.com/Jimmy-653/staemme-public/main/epm.js
-// @downloadURL  https://raw.githubusercontent.com/Jimmy-653/staemme-public/main/epm.js
-// @require		   https://raw.githubusercontent.com/Jimmy-653/staemme-public/main/staemme.js
+// @updateURL    https://raw.githubusercontent.com/Jimmy-653/staemme-public/main/tampermonkey.js
+// @downloadURL  https://raw.githubusercontent.com/Jimmy-653/staemme-public/main/tampermonkey.js
+// @require	     https://raw.githubusercontent.com/Jimmy-653/staemme-public/main/staemme.js
 // @homepage     https://raw.githubusercontent.com/Jimmy-653/staemme-public/main/README.md
 // @supportURL   https://discordapp.com/users/266279831618977792
 // ==/UserScript==

57b1a62a7156b3a304795b8c8bf23c45c5f09e78
Jimmy <speckmaier58@gmail.com>
2024-05-09 10:46:27 +0000

    Update Build 1-abc

--- tampermonkey.js
+++ tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Stämme Bot
 // @namespace    https://raw.githubusercontent.com/Jimmy-653/staemme-public
-// @version      0.0.3
+// @version      0.0.1-1-abc
 // @description  Create a "Bot" menu in online game Tribal Wars to automate various game actions.
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @author       Jimmy653
